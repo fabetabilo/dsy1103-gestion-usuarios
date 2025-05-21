6956f291274d5a6e66ca6ff524288c842768298b
fabetabilo <fab.tabilo@duocuc.cl>
2025-05-21 16:51:06 -0400

    Modificacion: pom.xml, app.propperties, para usar ORACLE

---
+++ gestion-usuarios/src/main/java/com/gestion_usuarios/gestion_usuarios/model/Usuario.java
@@ -0,0 +1,25 @@
+package com.gestion_usuarios.gestion_usuarios.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;                          // idUsuario para ser identificado dentro del sistema
+
+
+
+}
