09764e4376662ae998ccfe780b6820fdb850364f
fabetabilo <fab.tabilo@duocuc.cl>
2025-05-22 22:32:01 -0400

    wena cabros, ahora funciona

--- gestion-usuarios/src/main/java/com/gestion_usuarios/gestion_usuarios/controller/UsuarioController.java
+++ gestion-usuarios/src/main/java/com/gestion_usuarios/gestion_usuarios/controller/UsuarioController.java
@@ -3,8 +3,13 @@ package com.gestion_usuarios.gestion_usuarios.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,7 +62,7 @@ public class UsuarioController {
      * @return ResponseEntity HTTP 201 Created con el usuario creado, o HTTP 400 Bad Request si no se puede crear el usuario
      */
     
-     @PostMapping
+    @PostMapping
     public ResponseEntity<Usuario> createUsuario(@RequestBody Usuario usuario) { 
         try { 
             Usuario nuevoUsuario = this.usuarioService.saveUsuario(usuario);
@@ -75,10 +80,10 @@ public class UsuarioController {
      * @return ResponseEntity HTTP 200 OK con el usuario actualizado, o HTTP 400 Bad Request si no se puede actualizar el usuario
      */
     @PutMapping("/{idUsuario}")
-    public ResponseEntity<Usuario> updateUsuario(@PathVariable Long idUsuario, @RequestBody Usuario usuario) { 
+    public ResponseEntity<Usuario> updateUsuario(@PathVariable Integer idUsuario, @RequestBody Usuario usuario) { 
         try { 
             Usuario usuarioExistente = this.usuarioService.findByIdUsuario(idUsuario);
-            //usuarioExistente.setIdUsuario(idUsuario); FALTA
+            usuarioExistente.setIdUsuario(idUsuario);
             usuarioExistente.setRunUsr(usuario.getRunUsr());
             usuarioExistente.setSnombreUsr(usuario.getSnombreUsr());
             usuarioExistente.setPnombreUsr(usuario.getPnombreUsr());
@@ -89,13 +94,18 @@ public class UsuarioController {
             usuarioExistente.setCargo(usuario.getCargo());
 
             this.usuarioService.saveUsuario(usuarioExistente);
+
             return ResponseEntity.ok(usuarioExistente);
+
             
         } catch (Exception e) {
             // Si no encuentra el usuario en la base de datos
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+
         }
     }
+
+    
 
 
 
